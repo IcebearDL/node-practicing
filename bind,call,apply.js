27c4b3a44b5a8f7479d3169dbd5743ed6aca80ed
IcebearDL <u201616984@hust.edu.cn>
2019-09-09 22:27:58 +0800

    test

--- bind,call,apply.js
+++ bind,call,apply.js
@@ -1,24 +1,24 @@
 // 写bind,call,apply的原生js
 
-Function.prototype.mybind = function(){
+Function.prototype.mybind = function () {
     let obj = arguments[0] ? arguments[0] : window;
     obj.fn = this;
-    let args = arguments[1] ? Array.prototype.slice.call(arguments,1) : [];
+    let args = arguments[1] ? Array.prototype.slice.call(arguments, 1) : [];
     //bind返回新函数,并不执行
     let result = () => obj.fn(...args);
     return result;
 }
 
-Function.prototype.mycall = function(){
+Function.prototype.mycall = function () {
     let obj = arguments[0] ? arguments[0] : window;
     obj.fn = this;
-    let args = arguments[1] ? Array.prototype.slice.call(arguments,1) : [];
+    let args = arguments[1] ? Array.prototype.slice.call(arguments, 1) : [];
     let result = eval('obj.fn(...args)');
     delete obj.fn;
     return result;
 }
 
-Function.prototype.myapply = function(){
+Function.prototype.myapply = function () {
     let obj = arguments[0] ? arguments[0] : window;
     obj.fn = this;
     //args是传递过来的参数数组
@@ -31,13 +31,13 @@ Function.prototype.myapply = function(){
 
 // ----------------------------------------------------------------------------------
 
-function test(a,b,c,d){
+function test(a, b, c, d) {
     console.log(this);
-    console.log(a , b , c, d);
+    console.log(a, b, c, d);
 }
 
-let wxy = {name:'wxy',age:18,lover:'dl'};
+let wxy = { name: 'wxy', age: 18, lover: 'dl' };
 
-test.mycall(wxy,90,12,'??','sdf');
+test.mycall(wxy, 90, 12, '??', 'sdf');
 
 // console.log([].concat.mybind([1,2,3],[2,3,4]))
